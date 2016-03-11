4602ad5876679bc6ac0566ea28c7ac0dcf5731ca
Max Brauer <max@transcode.de>
2016-03-11 23:42:09 +0100

    Refacoted Scene object: directly added spritegroup.

--- burningkingdoms/game/scenes/core.py
+++ burningkingdoms/game/scenes/core.py
@@ -1,8 +1,12 @@
+import pygame
+
+
 class Scene(object):
     def __init__(self, fill, game, screen):
         self.fill = fill
         self.game = game
         self.screen = screen
+        self.sprites = pygame.sprite.Group()
 
     def render(self, screen):
         raise NotImplementedError
